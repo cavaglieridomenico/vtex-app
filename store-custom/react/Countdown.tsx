b97a64f2b741c0ac3d60633b72dcb0e461ee52fb
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-07-12 09:06:04 +0200

    Feature working countdown

--- store-custom/react/Countdown.tsx
+++ store-custom/react/Countdown.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {TimeSplit} from "./typings/global";
+import {tick, getTwoDaysFromNow} from "./utils/time";
 
 interface CountdownProps {
   targetDate: string
 }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate}: CountdownProps) => {
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate = DEFAULT_TARGET_DATE}) => {
+
+  const [remainingTime, setTime] = useState<TimeSplit>({
+    hours: '00',
+     minutes: '00',
+     seconds: '00'
+  });
+
+  tick(targetDate, setTime);
+
   return(
     <div>
-      <h1>{targetDate}</h1>
+      <h1>{`${remainingTime.hours}:${remainingTime.minutes}:${remainingTime.seconds}`}</h1>
     </div>
   )
 }
