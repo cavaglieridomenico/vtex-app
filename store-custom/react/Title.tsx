de6b67665415d90fd9ef6e7a13fc6b4e1b81a2b1
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-07-12 18:09:23 +0200

    Feature Title Component

---
+++ store-custom/react/Title.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FormattedMessage } from "react-intl";
+import { useCssHandles } from "vtex.css-handles";
+
+interface TitleProps {
+    title: string;
+}
+
+const CSS_HANDLES = ["title"];
+
+const Title: StorefrontFunctionComponent<TitleProps> = ({title}) => {
+
+    const handles = useCssHandles(CSS_HANDLES);
+
+    const titleText = title || <FormattedMessage id="editor.countdown.title" />
+
+    return (
+    <div className={`${handles.title} t-heading-2 fw3 w-100 c-muted-1 db tc`}>{titleText}</div>
+    )
+
+    
+};
+
+Title.schema = {
+    title: "editor.countdown.title.title",
+    description: "editor.countdown-title.description",
+    type: "object",
+    properties: {
+        title: {
+            title: "Title field",
+            description: "Title related to countdown",
+            type: "string",
+            default: null
+        }
+    } 
+}
+
+export default Title;
